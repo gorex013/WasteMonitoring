fdaee59176be480f2e701ba454691a98a3fba2c3
Vasile Rotaru <vasilerotaru38@gmail.com>
2021-10-06 22:03:06 +0300

    Now instead of `render` for template views there is used object method `render_to_response`.

--- waste_monitoring/views.py
+++ waste_monitoring/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
@@ -6,4 +5,4 @@ class HomeView(TemplateView):
     template_name = 'home.html'
 
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name)
+        return self.render_to_response({})
