890f59537afae7e804464c6ceb8a606c6e810ee9
Vasile Rotaru <vasilerotaru38@gmail.com>
2021-11-11 10:17:26 +0200

    Trying modal features from Bootstrap.

--- waste_monitoring/urls.py
+++ waste_monitoring/urls.py
@@ -14,14 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.shortcuts import redirect
 from django.urls import path, include
 
-from waste_monitoring.views import HomeView
+from waste_monitoring.views import HomeView, SignupView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),
-    path('signup/', lambda request, *args, **kwargs: redirect('home'), name='signup'),
+    path('signup/', SignupView.as_view(), name='signup'),
     path('', include('django.contrib.auth.urls'))
 ]
--- waste_monitoring/views.py
+++ waste_monitoring/views.py
@@ -1,4 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
 from django.views.generic import TemplateView
+
+from waste_monitoring.forms import SignUpForm
 
 
 class HomeView(TemplateView):
@@ -6,3 +10,24 @@ class HomeView(TemplateView):
 
     def get(self, request, *args, **kwargs):
         return self.render_to_response({})
+
+
+class SignupView(TemplateView):
+    template_name = 'registration/signup.html'
+    user_form = SignUpForm()
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {
+            'form': self.user_form,
+        }, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self.user_form = SignUpForm(request.POST)
+        if self.user_form.is_valid():
+            user = self.user_form.save(commit=False)
+            user.email = self.user_form.data['username']
+            user.save()
+            return HttpResponse(status=301)
+        return render(request, self.template_name, {
+            'form': self.user_form,
+        }, *args, **kwargs)
